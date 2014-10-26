1acee42b1494609f096bcb729f467da63936f38d
Igor <igor.zc@gmail.com>
2014-10-26 15:58:07 +0200

    added "Select Contact" button and android manifest file

--- sms2/sms2/MainActivity.cs
+++ sms2/sms2/MainActivity.cs
@@ -6,13 +6,16 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Telephony;
+using Android.Provider;
+using Android.Util;
 
 namespace sms2
 {
 	[Activity (Label = "sms2", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		const int SELECT_CONTACT_SUCCESS_RESULT = 101;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -25,11 +28,32 @@ namespace sms2
 			// and attach an event to it
 			Button selectContactButton = FindViewById<Button> (Resource.Id.selectContactButton);
 
-/*
+
 			selectContactButton.Click += delegate {
-				selectContactButton.Text = string.Format ("{0} clicks!", count++);
+				//Create a new intent for choosing a contact
+				var contactPickerIntent = new Intent (Intent.ActionPick, Android.Provider.ContactsContract.Contacts.ContentUri);
+				contactPickerIntent.SetType (ContactsContract.CommonDataKinds.Phone.ContentType); //(Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+				// Start the contact picker expecting a result with the resultCode '101'  
+				StartActivityForResult (contactPickerIntent, SELECT_CONTACT_SUCCESS_RESULT);
 			};
-*/			
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+
+			switch (requestCode) {
+			case SELECT_CONTACT_SUCCESS_RESULT:
+				SelectContactHandling (resultCode, data);
+				break;
+			}
+		}
+
+		void SelectContactHandling(Result resultCode, Intent data)
+		{
+			if (resultCode == Result.Ok && data!=null) {
+			}
+
 		}
 	}
 }
